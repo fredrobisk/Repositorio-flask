a6288310a4463839151ef77c0e2de42b6b110e28
frederico <fredericobrigido@gmail.com>
2026-08-19 14:44:10 -0300

    adicionando novas branches

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import sys
+
 def main():
-    print("Hello World")
+    if len(sys.argv) > 1:
+        print("ola,", sys.argv[1] + '!')
+    else:
+        print("Ola mundo")
 
 if __name__ == "__main__":
     main()
